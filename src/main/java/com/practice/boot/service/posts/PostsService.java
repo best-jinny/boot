efbadc09df3f053c2ff3891e0f48d24e5d39c45e
best-jinny <iwilldotherightthings@gmail.com>
2022-08-30 22:28:30 +0900

    posts 등록, 조회 화면 개발

--- src/main/java/com/practice/boot/service/posts/PostsService.java
+++ src/main/java/com/practice/boot/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package com.practice.boot.service.posts;
 import com.practice.boot.domain.posts.Posts;
 import com.practice.boot.domain.posts.PostsRepository;
 import com.practice.boot.web.PostsUpdateRequestDto;
+import com.practice.boot.web.dto.PostsListResponseDto;
 import com.practice.boot.web.dto.PostsResponseDto;
 import com.practice.boot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)     // postsRepository 결과로 넘어온 Posts의 Stream을
+                .collect(Collectors.toList());      // map을 통해 PostsListResponseDto로 변환하여 List로 반환
+    }
+
 }
