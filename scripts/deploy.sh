36cd9b771b745383aa7352c27c2c6a4aba71f3d0
best-jinny <iwilldotherightthings@gmail.com>
2022-09-04 22:54:59 +0900

    배포 자동화 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ CURRENT_PID=$(pgrep -fl boot | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -25,7 +25,7 @@ echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> $JAR_NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
