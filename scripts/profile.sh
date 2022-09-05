231a1df93a766f3d77ff9b9fdb0c50e95213b941
best-jinny <iwilldotherightthings@gmail.com>
2022-09-05 23:37:24 +0900

    오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -4,7 +4,7 @@
 
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400보다 크면(즉, 40x,50x 에러 모두 포함)
     then
@@ -17,7 +17,7 @@ function find_idle_profile()
     then
       IDLE_PROFILE=real2
     else
-      IDEL_PROFILE=real1
+      IDLE_PROFILE=real1
     fi
 
     echo "${IDLE_PROFILE}"
